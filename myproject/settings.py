f80172f11b5e57ab3405fa3c74ed28f58a3924df
unknown <rinil.kr@rqsolutions.com>
2023-01-24 14:25:25 +0530

    more changes

--- myproject/settings.py
+++ myproject/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vm99juc#3xbbs$lc9@q^bwi-+4ryrh8^7*_h7wnm#3!c*w%fp%'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -87,9 +87,9 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'EmpDb',
-        'USER': 'postgres',
-        'PASSWORD':'rinildb',
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD':env('DATABASE_PASS'),
         'HOST':'localhost',
         'PORT':'5432',
     }
